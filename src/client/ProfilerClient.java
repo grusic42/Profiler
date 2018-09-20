0a720463e5292e393c9c64cd33fceef40f1efc7d
Jakob Sand Svarstad <watashiwaore@gmail.com>
2018-09-20 08:49:37 +0200

    fixed merge conflict thing i did it right
    but i am not sure

--- src/client/ProfilerClient.java
+++ src/client/ProfilerClient.java
@@ -21,48 +21,6 @@ public class ProfilerClient {
 
 	public static class Input implements Runnable {
 
-<<<<<<< HEAD
-        public void run() {
-            Scanner in = new Scanner(System.in);
-            while (true) {
-                String s = in.nextLine();
-                parse(s);
-            }
-        }
-
-        void parse(String str) {
-            if (str.startsWith(CMD_TIMES_PLAYED)) {
-            	String song = str.substring(CMD_TIMES_PLAYED.length());
-            	timesPlayed(song);
-            } else if (str.startsWith(CMD_USER_TIMES_PLAYED)) {
-            	String[] sp = str.split(" ");
-            	String user = sp[1];
-            	String song = sp[2];
-            	timesPlayedByUser(user, song);
-            } else if (str.startsWith(CMD_SONG_TOP_3)) {
-                String song = str.substring(CMD_SONG_TOP_3.length());
-                topThree(song);
-            } else if (str.startsWith(CMD_HELP)) {
-            	help();
-            } else if (str.startsWith(CMD_QUIT)) {
-                System.exit(0);
-            } else {
-                profilerImpl.sendMessage(str);
-            }
-        }
-
-        void help() {
-            String str = "Commands:\n" +
-            		CMD_TIMES_PLAYED + "[song]\n" +
-            		CMD_USER_TIMES_PLAYED + "[song] [user]\n" +
-            		CMD_SONG_TOP_3 + "[song]\n" +
-                    CMD_QUIT;
-            System.out.println(str);
-        }
-        
-        void loadPrompt() {
-        	try {
-=======
 		public void run() {
 			Scanner in = new Scanner(System.in);
 			while (true) {
@@ -74,15 +32,12 @@ public class ProfilerClient {
 		void parse(String str) {
 			if (str.startsWith(CMD_TIMES_PLAYED)) {
 				String song = str.substring(CMD_TIMES_PLAYED.length());
-				loadPrompt();
-				System.out.println("Song " + song + " played " + profilerImpl.getTimesPlayed(song) + " times.");
+				timesPlayed(song);
 			} else if (str.startsWith(CMD_USER_TIMES_PLAYED)) {
 				String[] sp = str.split(" ");
 				String user = sp[1];
 				String song = sp[2];
-				loadPrompt();
-				System.out.println("Song " + song + " played " + profilerImpl.getTimesPlayedByUser(user, song)
-						+ " times by user " + user + ".");
+				timesPlayedByUser(user, song);
 			} else if (str.startsWith(CMD_SONG_TOP_3)) {
 				String song = str.substring(CMD_SONG_TOP_3.length());
 				topThree(song);
@@ -103,61 +58,48 @@ public class ProfilerClient {
 
 		void loadPrompt() {
 			try {
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
+
 				Thread.sleep(5000);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-<<<<<<< HEAD
-        	System.out.println("...[working hard]...");
-        }
-        
-        void timesPlayed (String song) {
-        	long startTime = System.currentTimeMillis();
-        	loadPrompt();
-        	int timesPlayed = profilerImpl.getTimesPlayed(song);
-        	long elapsedTime = System.currentTimeMillis() - startTime;
-        	System.out.println("Song " + song + " played " + timesPlayed + " times.(" + elapsedTime  + "ms)");
-        	
-        	
-        }
-        
-        void timesPlayedByUser (String user, String song) {
-        	long startTime = System.currentTimeMillis();
-        	loadPrompt();
-        	int timesPlayed = profilerImpl.getTimesPlayedByUser(user, song);
-        	long elapsedTime = System.currentTimeMillis() - startTime;
-        	if (timesPlayed != 0) {
-        		System.out.println("Song " + song + " played " + timesPlayed + " times by user " + user + ".("+ elapsedTime  + "ms)");
-        	} else System.out.println("Song not played by this user");
-        }
-        
-        void topThree (String song) {
-        	long startTime = System.currentTimeMillis();
-        	loadPrompt();
-        	String[] top3 = (profilerImpl.getTopThreeUsersBySong(song)).split("\n");
-        	long elapsedTime = System.currentTimeMillis() - startTime;
-        	
-        	System.out.println("Song " + song  +" (" + elapsedTime  + "ms)");
-        	for (String go : top3) {
-        		String[] wow = go.split("\t");
-            	System.out.println("User " + wow[0] + " listened " + wow[2] + " times.");
-        	}
-        }
-=======
+
 			System.out.println("...[working hard]...");
 		}
 
-		void topThree(String song) {
-			String[] top3 = (profilerImpl.getTopThreeUsersBySong(song)).split("\n");
+		void timesPlayed(String song) {
+			long startTime = System.currentTimeMillis();
 			loadPrompt();
-			System.out.println("Song " + song);
+			int timesPlayed = profilerImpl.getTimesPlayed(song);
+			long elapsedTime = System.currentTimeMillis() - startTime;
+			System.out.println("Song " + song + " played " + timesPlayed + " times.(" + elapsedTime + "ms)");
+
+		}
+
+		void timesPlayedByUser(String user, String song) {
+			long startTime = System.currentTimeMillis();
+			loadPrompt();
+			int timesPlayed = profilerImpl.getTimesPlayedByUser(user, song);
+			long elapsedTime = System.currentTimeMillis() - startTime;
+			if (timesPlayed != 0) {
+				System.out.println("Song " + song + " played " + timesPlayed + " times by user " + user + ".("
+						+ elapsedTime + "ms)");
+			} else
+				System.out.println("Song not played by this user");
+		}
+
+		void topThree(String song) {
+			long startTime = System.currentTimeMillis();
+			loadPrompt();
+			String[] top3 = (profilerImpl.getTopThreeUsersBySong(song)).split("\n");
+			long elapsedTime = System.currentTimeMillis() - startTime;
+
+			System.out.println("Song " + song + " (" + elapsedTime + "ms)");
 			for (String go : top3) {
 				String[] wow = go.split("\t");
-				System.out.println("User " + wow[0] + " listened " + wow[2] + " times");
+				System.out.println("User " + wow[0] + " listened " + wow[2] + " times.");
 			}
 		}
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
 	}
 
 	public static void main(String[] args) {
--- src/server/ProfilerServant.java
+++ src/server/ProfilerServant.java
@@ -7,56 +7,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 import TasteProfile.ProfilerPOA;
 
 public class ProfilerServant extends ProfilerPOA {
-<<<<<<< HEAD
-	
-	/*
-		class UserCounter {
-			public String user_id;
-			public long song_play_time;
-		}
-		
-		class TopThree {
-			public UserCounter[] topThreeUsers;
-		}
-		
-		class SongProfile {
-			public long total_play_count;
-			public TopThree top_three_users;
-		}
-		 
-	private void loadCache() {
-		BufferedReader br = null;
-		try {
-			File file = new File("/root/Documents/INF5020/first assignment/train_triplets.txt");
-			
-			br = new BufferedReader(new FileReader(file));
-			//Map<String,int> userCounter = new HashMap<String,int>();
-						
-			String st;
-			while ((st = br.readLine()) != null) {
-				String[] tuple = st.split("\t");
-				String user = tuple[0];
-				String song = tuple[1];
-				int plays = Integer.parseInt(tuple[2]);
-				Map<String, Integer> userCounter = new HashMap<>();
-				userCounter.put(user, plays);
-					
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	}
-	*/
-	
-=======
 
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
+	/*
+	 * class UserCounter { public String user_id; public long song_play_time; }
+	 * 
+	 * class TopThree { public UserCounter[] topThreeUsers; }
+	 * 
+	 * class SongProfile { public long total_play_count; public TopThree
+	 * top_three_users; }
+	 * 
+	 * private void loadCache() { BufferedReader br = null; try { File file = new
+	 * File("/root/Documents/INF5020/first assignment/train_triplets.txt");
+	 * 
+	 * br = new BufferedReader(new FileReader(file)); //Map<String,int> userCounter
+	 * = new HashMap<String,int>();
+	 * 
+	 * String st; while ((st = br.readLine()) != null) { String[] tuple =
+	 * st.split("\t"); String user = tuple[0]; String song = tuple[1]; int plays =
+	 * Integer.parseInt(tuple[2]); Map<String, Integer> userCounter = new
+	 * HashMap<>(); userCounter.put(user, plays);
+	 * 
+	 * } } catch (IOException e) { e.printStackTrace(); } }
+	 */
+
 	@Override
 	public String sendMessage(String message) {
 		// TODO Auto-generated method stub
@@ -65,12 +42,9 @@ public class ProfilerServant extends ProfilerPOA {
 
 	@Override
 	public int getTimesPlayed(String song_id) {
-<<<<<<< HEAD
-		fakeNetworkLatency();
-		
-=======
 
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
+		fakeNetworkLatency();
+
 		BufferedReader br = null;
 		int sum = 0;
 
@@ -99,12 +73,9 @@ public class ProfilerServant extends ProfilerPOA {
 
 	@Override
 	public int getTimesPlayedByUser(String user_id, String song_id) {
-<<<<<<< HEAD
-		fakeNetworkLatency();
-		
-=======
 
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
+		fakeNetworkLatency();
+
 		BufferedReader br = null;
 		int sum = 0;
 
@@ -133,52 +104,21 @@ public class ProfilerServant extends ProfilerPOA {
 
 	@Override
 	public String getTopThreeUsersBySong(String song_id) {
-		
+
 		BufferedReader br = null;
 		ArrayList<String> matches = new ArrayList<String>();
 
 		try {
-<<<<<<< HEAD
-			fakeNetworkLatency();
-			File file = new File("/root/Documents/INF5020/first assignment/train_triplets.txt");
-			
-			br = new BufferedReader(new FileReader(file));
-			
-=======
+
 			File file = new File("root/../../train_triplets.txt");
 
 			br = new BufferedReader(new FileReader(file));
 
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
 			String st;
 			while ((st = br.readLine()) != null) {
 				String[] tuple = st.split("\t");
 				if (song_id.equals(tuple[1]))
 					matches.add(st);
-<<<<<<< HEAD
-				}
-			
-			// Sorts song array asc order
-			Collections.sort(matches, new Comparator<String>() {
-		        @Override
-		        public int compare(String o1, String o2) {
-		        	String a1 = o1.substring(60);
-		        	String a2 = o2.substring(60);
-		            return Integer.valueOf(a1).compareTo(Integer.valueOf(a2));
-		        }
-		    });
-			
-			// Sends back the list's last three users, i.e. the ones who has the most song plays
-			StringBuilder sb = new StringBuilder();
-			for (int i = matches.size()-3; i < matches.size(); i++) {
-			{
-			    sb.append(matches.get(i));
-			    sb.append("\n");
-			}
-			}
-			return sb.toString();
-		} catch (IOException e) {
-=======
 			}
 
 			// Sorts song array asc order
@@ -191,6 +131,8 @@ public class ProfilerServant extends ProfilerPOA {
 				}
 			});
 
+			// Sends back the list's last three users, i.e. the ones who has the most song
+			// plays
 			StringBuilder sb = new StringBuilder();
 			for (int i = matches.size() - 3; i < matches.size(); i++) {
 				{
@@ -200,27 +142,18 @@ public class ProfilerServant extends ProfilerPOA {
 			}
 			return sb.toString();
 		} catch (IOException e) {
+
 			e.printStackTrace();
 		} finally {
 			try {
 				br.close();
 			} catch (IOException e) {
->>>>>>> aa79db621c5dfee523195385aacc3229bdf46c4a
 				e.printStackTrace();
-		} 
-		finally {
-				try {
-					br.close();
-				} catch 
-				(IOException e) {
-					e.printStackTrace();
-				}
+			}
 		}
 		return null;
 	}
-	
-	
-	
+
 	private void fakeNetworkLatency() {
 		try {
 			Thread.sleep(80);
@@ -228,7 +161,5 @@ public class ProfilerServant extends ProfilerPOA {
 			e.printStackTrace();
 		}
 	}
-	
-	
 
 }
